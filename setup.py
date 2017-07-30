ecb0434551b9572ef4876e033a07fe70d84fb054
kmyounes <kmyounes@protonmail.com>
2017-07-30 19:42:08 +0100

    Added installer

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='docker-launcher',
+      version='0.1',
+      description='Small script for managing docker containers launch',
+      url='https://github.com/kmyounes/docker-launcher',
+      author='KASMI Mohamed Younes',
+      author_email='kmyounes@protonmail.com',
+      scripts=['docker-launcher/docker-launcher']
+      )
